6f901b681528e74dddf3c831a87d188d6e5a8a5a
Diva Nadya Pasha Ramadhani <141887234+Divauu@users.noreply.github.com>
2024-01-10 20:56:31 +0700

    Add files via upload

---
+++ Bus.java
@@ -0,0 +1,10 @@
+package ClassPBO.Enkapsulasi.Latihan1;
+
+public class Bus {
+    public int penumpang, maxPenumpang;
+
+    public void cetak(){
+        System.out.println("Penumpang sekarang = "+penumpang);
+        System.out.println("Penumpang seharusnya adalah = "+maxPenumpang);
+    }
+}
